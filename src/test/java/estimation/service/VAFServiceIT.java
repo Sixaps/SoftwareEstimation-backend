a6b26d0ab082607e74ba0d3fce7853ac62174525
Sixaps <sixaps6@gamil.com>
2018-07-01 22:49:02 +0800

    using surefire to identity unit test and integration test

--- src/test/java/estimation/service/VAFServiceIT.java
+++ src/test/java/estimation/service/VAFServiceIT.java
@@ -1,5 +1,6 @@
 package estimation.service;
 
+import estimation.Application;
 import estimation.DAO.VAFDao;
 import estimation.bean.VAF;
 import estimation.service.VAFService;
@@ -8,8 +9,10 @@ import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.context.web.WebAppConfiguration;
 
 import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
@@ -22,7 +25,8 @@ import static org.junit.Assert.assertTrue;
  * @since <pre>���� 8, 2018</pre>
  */
 @RunWith(SpringRunner.class)
-@SpringBootTest
+@SpringBootTest//(classes = Application.class)
+@EnableAutoConfiguration
 public class VAFServiceIT {
 
 
